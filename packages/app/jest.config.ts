ae09f36a7402894d2e2b0a8162240f6d038212ab
krizz <christian@o3tt3rli.com>
2023-05-06 09:03:58 +0200

    update nx workspace

--- packages/app/jest.config.ts
+++ packages/app/jest.config.ts
@@ -4,19 +4,5 @@ export default {
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../coverage/packages/app',
-  transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$'
-      }
-    ]
-  },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment'
-  ]
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)']
 };
